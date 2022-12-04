79f1eaef80d5fe68fec2cbfb8f8accd9e355e0f6
omguptalko <119674842+omguptalko@users.noreply.github.com>
2022-12-04 12:36:49 +0530

    Add files via upload

---
+++ tab.c
@@ -0,0 +1,17 @@
+/* table */
+#include<stdio.h>
+void main()
+{
+ int a=25;
+ printf("%d\n",a*1);
+ printf("%d\n",a*2);
+ printf("%d\n",a*3);
+ printf("%d\n",a*4);
+ printf("%d\n",a*5);
+ printf("%d\n",a*6);
+ printf("%d\n",a*7);
+ printf("%d\n",a*8);
+ printf("%d\n",a*9);
+ printf("%d\n",a*10);
+ return 0;
+} 
